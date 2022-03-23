3c6271d0a1baa80ca010394690c6ad7674aa160c
fangwei <4027164@qq.com>
2022-03-24 00:13:46 +0800

    Add files via upload

---
+++ src/NewGeneralListWpf/NGSL/NewGeneralServiceList.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace NewGeneralListWpf.NGSL
+{
+    internal class NewGeneralServiceList
+    {
+        private List<string> ngsl_words;
+        private string? ngsl_filename;
+        private Random random;
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="ngsl_filename">录入文件名词 NGSL_word2818.txt</param>
+        public NewGeneralServiceList(string ngsl_filename)
+        {
+            random = new Random(Guid.NewGuid().GetHashCode());
+            ngsl_words = new List<string>();
+            this.ngsl_filename = ngsl_filename;
+            InitWords();
+        }
+        public List<string> Words { get => ngsl_words; }
+        public string AnyOneWord()
+        {
+            return ngsl_words[random.Next(0, ngsl_words.Count - 1)];
+        }
+        public string[] AnyWords(int count)
+        {
+            string[] words = new string[count];
+            for (int i = 0; i < count; i++)
+            {
+                words[i] = AnyOneWord();
+            }
+            return words;
+        }
+        private void InitWords()
+        {
+            if (ngsl_filename == null) throw new ArgumentNullException(nameof(ngsl_filename) + "is null! please select a words txt file.");
+            using (TextReader sr = new StreamReader(ngsl_filename))
+            {
+                string? line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    ngsl_words.Add(line.Trim());
+                }
+            }
+        }
+    }
+}
---
+++ src/NewGeneralListWpf/Window1.xaml.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace NewGeneralListWpf
+{
+    /// <summary>
+    /// Window1.xaml 的交互逻辑
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        private string ngsl_file = String.Empty;
+        private string ngsl_pfile = String.Empty;
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_selectfile_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+            btn_convert.IsEnabled = false;
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == true)
+            {
+                if (button.Name == "btn_selectfilengsl")
+                {
+                    ngsl_file = ofd.FileName;
+                    txt_ngsl_full.Text = LoadNGSL(ngsl_file);
+                }
+                else if (button.Name == "btn_selectfilengsl_p")
+                {
+                    ngsl_pfile = ofd.FileName;
+                    txt_ngsl_p.Text = LoadNGSL(ngsl_pfile);
+                }
+            }
+            btn_convert.IsEnabled = true;
+        }
+
+        private void btn_convert_Click(object sender, RoutedEventArgs e)
+        {
+            List<string> listfull = new NGSL.NewGeneralServiceList(ngsl_file).Words;
+            List<string> listp = new NGSL.NewGeneralServiceList(ngsl_pfile).Words;
+            List<String> listwithoutp = new List<string>();
+            foreach (string s in listfull)
+            {
+                //已知问题:
+                //Contains只是包含,并不能完全匹配,未找到优化算法!
+                if (!listp.Contains(s))
+                {
+                    listwithoutp.Add(s);
+                }
+            }
+            StringBuilder sb = new StringBuilder();
+            foreach (string s in listwithoutp)
+            {
+                sb.AppendLine(s);
+            }
+            txt_ngsl_withoutp.Text = sb.ToString().TrimEnd();
+
+            txt_ngsl_full.Background = txt_ngsl_p.Background = txt_ngsl_withoutp.Background = Brushes.LightYellow;
+
+            lab_ngsl_full.Text = "NGSL FUll:" + listfull.Count;
+            lab_ngsl_p.Text = "NGSL-Spoken:" + listp.Count;
+            lab_ngsl_withoutp.Foreground = Brushes.Red;
+            lab_ngsl_withoutp.Text = $"NGSL Without Spoken:{listwithoutp.Count}(truly:{listfull.Count - listp.Count}),error!";
+        }
+        private string LoadNGSL(string filename)
+        {
+            StringBuilder sb = new StringBuilder();
+            using (StreamReader sr = new StreamReader(filename))
+            {
+                sb.Append(sr.ReadToEnd());
+            }
+            return sb.ToString();
+        }
+    }
+}
